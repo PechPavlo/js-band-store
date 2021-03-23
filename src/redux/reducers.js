37f745adc2727f1efe0801ed985de284b39d0617
PechPavlo <61708124+PechPavlo@users.noreply.github.com>
2021-03-23 12:16:54 +0200

    clean up in redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -23,54 +23,6 @@ const reducer = (state, action) => {
         error: action.payload.error,
       };
 
-    case 'UPDATE_MY_EVENTS':
-      return {
-        ...state,
-        myEvents: action.payload,
-      };
-
-    case 'UPDATE_FILTERED_BY':
-      return {
-        ...state,
-        filteredByUser: action.payload,
-      };
-
-    case 'UPDATE_USERS':
-      return {
-        ...state,
-        users: action.payload,
-      };
-
-    case 'UPDATE_IS_AUTHORIZED':
-      return {
-        ...state,
-        isAuthorized: action.payload,
-      };
-
-    case 'UPDATE_EVENT_TO_ADD':
-      return {
-        ...state,
-        eventToAdd: action.payload,
-      };
-
-    case 'UPDATE_EVENT_TO_DELETE':
-      return {
-        ...state,
-        eventToDelete: action.payload,
-      };
-
-    case 'UPDATE_IS_EVENTS_UPDATED':
-      return {
-        ...state,
-        isEventsUpdated: action.payload,
-      };
-
-    case 'UPDATE_CURRENT_USER':
-      return {
-        ...state,
-        currentUser: action.payload,
-      };
-
     default:
       return { ...state };
   }
